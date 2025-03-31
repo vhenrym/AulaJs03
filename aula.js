fe7f1a23d232a7c23c5f1c0971af8616dd24c98d
Victor Henry <159193296+vhenrym@users.noreply.github.com>
2025-03-31 09:22:37 -0300

    Aula de JS

---
+++ aula.js
@@ -0,0 +1,12 @@
+var nome = prompt('Insira seu nome:');
+alert("Olá " + nome + " seja bem vindo!");
+
+var num = prompt('Insira um número:');
+var valor1 = parseInt(num);
+
+var num2 = prompt('Insira o segundo número para realizar a soma:');
+var valor2 = parseInt(num2);
+
+var soma = valor1 + valor2
+
+alert("O resultado da soma dos números será: " + soma)
